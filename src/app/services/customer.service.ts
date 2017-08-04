a4d324a153d64aafcc90f3c0cf3396b1938d0e4d
Alex Turevski <me@turevski.com>
2017-08-05 00:33:10 +0300

    local storage

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -10,8 +10,50 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class CustomerService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    var dataCustomers = localStorage.getItem('customers');
+    var dataCustomerNavigations = localStorage.getItem('customerNavigations');
 
+    if (dataCustomers) {
+      this.fillCustomers(JSON.parse(dataCustomers));
+    }
+    if (dataCustomerNavigations) {
+      this.fillCustomerNavigations(JSON.parse(dataCustomerNavigations));
+    }
+  }
+
+  private customers: Customer[];
+  private customerNavigations: CustomerNavigation[];
+
+  private fillCustomers(data) {
+    this.customers = [];
+    (data as any[]).forEach((item) => {
+      this.customers.push(
+        new Customer(
+          item.id,
+          item.firstName,
+          item.lastName,
+          new Date(item.birthday),
+          item.gender,
+          new Date(item.lastContactDate),
+          item.lifetimeValue
+        )
+      );
+    });
+  }
+
+  private fillCustomerNavigations(data) {
+    this.customerNavigations = [];
+    (data as any[]).forEach((item) => {
+      this.customerNavigations.push(
+        new CustomerNavigation(
+          this.customers.find(c => (c.id == item.customerId)),
+          item.pages,
+          new Date(item.timeStamp)
+        )
+      );
+    });
+  }
 
   LoadInitData(): Observable<any> {
 
@@ -21,38 +63,18 @@ export class CustomerService {
           observer.next("OK");
           observer.complete();
         });
-      this.customers = [];
-      (data.customers as any[]).forEach((item) => {
-        this.customers.push(
-          new Customer(
-            item.id,
-            item.firstName,
-            item.lastName,
-            new Date(item.birthday),
-            item.gender,
-            new Date(item.lastContactDate),
-            item.lifetimeValue
-          )
-        );
-      });
-      this.customerNavigations = [];
-      (data.customerNavigations as any[]).forEach((item) => {
-        this.customerNavigations.push(
-          new CustomerNavigation(
-            this.customers.find(c => (c.id == item.customerId)),
-            item.pages,
-            new Date(item.timeStamp)
-          )
-        );
-      });
+
+      this.fillCustomers(data.customers);
+      this.fillCustomerNavigations(data.customerNavigations);
+
+      localStorage.setItem('customers', JSON.stringify(this.customers));
+      localStorage.setItem('customerNavigations', JSON.stringify(this.customerNavigations));
+
       return "OK";
     });
 
     return result;
   }
-
-  customerNavigations: CustomerNavigation[];
-  customers: Customer[];
 
   public getCustomers(): Observable<Customer[]> {
     return this.LoadInitData().map(ok => this.customers);
